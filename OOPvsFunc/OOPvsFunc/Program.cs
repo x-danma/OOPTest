a9f97ddea2b869cbdf44ef0756fbd961fc35d5ef
Daniel Mårtensson <daniel.martensson89@outlook.com>
2017-04-05 19:21:18 +0200

    Remove func project, add class diagram

--- OOPvsFunc/OOPvsFunc/Program.cs
+++ OOPvsFunc/OOPvsFunc/Program.cs
@@ -13,6 +13,22 @@ namespace OOPvsFunc
             Console.WriteLine("Hello World");
             //Vad göra? ett program som kanske tittar på en lista av objekt och visar upp dem i en tabell? CRUD på det?
             // Eller kanske ett kortspel? med randomisering för Dice-throws?
+
+            /*Programmet ska hantera ett personregister
+             * För listan kan man 
+             *  - Lägga till
+             *  - Ta bort
+             * en person
+             * 
+             * En person har
+             *  förnamn
+             *  efternamn
+             *  ålder
+             *  ssn
+             * 
+             * 
+             * 
+             * */
         }
     }
 }
