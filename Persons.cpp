9b188587f614483659cedc03ceb3b2eecee947d0
Christopher Mårtensson <cribalik@gmail.com>
2017-04-05 20:22:39 +0200

    Fixed some compiler warnings

--- Persons.cpp
+++ Persons.cpp
@@ -1,13 +1,15 @@
 #include <cstdio>
 #include <cstring>
 using namespace std;
+
 struct Person
 {
 	char firstName[32];
 	char surName[32];
 	int age;
-	char socialNumber[32];
+	char ssn[32];
 };
+
 int main(int argc, char const *argv[])
 {
 	Person people[64];
@@ -21,38 +23,39 @@ int main(int argc, char const *argv[])
 			"Enter a to add person\n"
 			"Enter d to delete a person by ssn\n"
 			);
+		char command;
 		while((command = getchar()) == '\n');
 		while(getchar() != '\n');
 		switch (command)
 		{
-			case 'q':return 0;
+			case 'q': return 0;
 			case 'p':
 			{
 				printf("Your people are:\n");
 				for (int i = 0; i < numPeople; ++i)
-					printf("- %s %s %i %s \n",people[i].firstName,people[i].surName, people[i].age, people[i].socialNumber);
-			}break;
+					printf("- %s %s %i %s \n",people[i].firstName,people[i].surName, people[i].age, people[i].ssn);
+			} break;
 			case 'a':
 			{
 				puts("Enter firstName");
-				scanf("%32s", &people[numPeople].firstName);
+				scanf("%32s", people[numPeople].firstName);
 				puts("Enter surName");
-				scanf("%32s", &people[numPeople].surName);
+				scanf("%32s", people[numPeople].surName);
 				puts("Enter age");
 				scanf("%i", &people[numPeople].age);
-				puts("Enter socialNumber");
-				scanf("%32s", &people[numPeople].socialNumber);
+				puts("Enter social security number");
+				scanf("%32s", people[numPeople].ssn);
 				numPeople++;
-			}break;
+			} break;
 			case 'd':
 			{
-				puts("Enter ssn");
+				puts("Enter social security number");
 				char ssn[32];
 				scanf("%32s", ssn);
 				for (int i = 0; i < numPeople; ++i)
-					if (strcmp(people[i].socialNumber, ssn) == 0)
+					if (strcmp(people[i].ssn, ssn) == 0)
 						people[i] = people[--numPeople];
-			}break;
+			} break;
 		}
 	}
 	return 0;
